9e53be91daccb4b38a67b535b3fc068c806d0427
Wael Ben Taleb <waelben7@gmail.com>
2018-11-22 16:56:33 +0100

    init commit

---
+++ app/src/main/java/tn/igc/projectone/MainActivity.java
@@ -0,0 +1,16 @@
+package tn.igc.projectone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// Hello from the other side
+// **** *** GitLab
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
