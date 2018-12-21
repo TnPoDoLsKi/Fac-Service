df33f652bf25ee41de928bfd46c5e7fcbcc6a570
wassim <wassim22m@gmail.com>
2018-12-21 21:00:45 +0100

    Bottom bar navigation

--- app/src/main/java/tn/igc/projectone/MainActivity.java
+++ app/src/main/java/tn/igc/projectone/MainActivity.java
@@ -1,30 +1,19 @@
 package tn.igc.projectone;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 // Hello from the other side
-
-
 // From Wael
-
-
-
 // From Achouri
-
 //From Mariam
 //from masmoudi
 // Hello from wassim ^_^
-// Nouri says Hi 
-
-
-
-
-
-
-
-
-
+// Nouri says Hi
 //from chaima
 public class MainActivity extends AppCompatActivity {
 
@@ -32,5 +21,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomBar);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new homeFragment());
+        bottomNavigationView.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) navigationItemReselectedListener);
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemReselectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null ;
+                    switch (menuItem.getItemId()){
+
+                        case R.id.home_button:
+                            selectedFragment = new homeFragment();
+                            break;
+                        case R.id.add_button:
+                            selectedFragment = new AddFragment();
+                            break;
+                        case R.id.parametre_button:
+                            selectedFragment = new settingFragment();
+                            break;
+                        case R.id.search_button:
+                            selectedFragment = new searchFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,selectedFragment).commit();
+                    return  true ;
+                }
+
+
+    };
 }
---
+++ app/src/main/java/tn/igc/projectone/searchFragment.java
@@ -0,0 +1,27 @@
+package tn.igc.projectone;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class searchFragment extends Fragment {
+
+    @Nullable
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+
+}
