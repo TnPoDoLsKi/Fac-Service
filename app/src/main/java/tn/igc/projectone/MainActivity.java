a31cdc78c3ef30105ba4a0d94531feb11b0b0bcc
myslo <farahbedoui96@gmail.com>
2018-12-21 20:53:50 +0100

    ok

--- app/src/main/java/tn/igc/projectone/MainActivity.java
+++ app/src/main/java/tn/igc/projectone/MainActivity.java
@@ -2,35 +2,21 @@ package tn.igc.projectone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 // Hello from the other side
 
 
 // From Wael
-
-
-
 // From Achouri
-
+//from Farah
 //From Mariam
 //from masmoudi
 // Hello from wassim ^_^
-// Nouri says Hi 
-
-
-
-
-
-
-
-
-
+// Nouri says Hi
 //from chaima
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
