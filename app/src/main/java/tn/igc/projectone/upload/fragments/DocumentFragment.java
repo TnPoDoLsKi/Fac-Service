6073236e2659054bf34f2f8e46987c19bab0702c
mariamdammak <mariamdammak6@gmail.com>
2019-03-05 19:34:39 +0100

    margeee

--- app/src/main/java/tn/igc/projectone/upload/fragments/DocumentFragment.java
+++ app/src/main/java/tn/igc/projectone/upload/fragments/DocumentFragment.java
@@ -29,7 +29,7 @@ import tn.igc.projectone.upload.Api.APIInterface;
 
 public class DocumentFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBERj
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
